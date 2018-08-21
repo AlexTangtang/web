decfad4c30e5ad3c0d4ccf165d13fadfbbe2e872
tangjie <alex_tang_151@163.com>
2018-08-21 16:21:22 +0800

    新添加首页以及tabbar

---
+++ bzApp/js/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by AlexTang on 2018/8/21.
+ */
+function addFirstPage() {
+    var superDiv = document.getElementById('tabSuper')
+    var homePage = document.getElementById("homePage")
+    superDiv.innerHTML = '<div>这是内容<div/>'//homePage
+}
---
+++ bzApp/js/userCenter.js
@@ -0,0 +1,12 @@
+/**
+ * Created by AlexTang on 2018/8/17.
+ */
+function  showShare() {
+    // alert('我要分享')
+    var sharePopView = document.getElementById('sharePopView')
+    sharePopView.style.display = 'inline-block'
+}
+function closeAction() {
+    var sharePopView = document.getElementById('sharePopView')
+    sharePopView.style.display = 'none'
+}
